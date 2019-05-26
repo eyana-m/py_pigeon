fdaaf1003fe52ac4d9356f1b089c50506e3ab136
eyana-rc <eyana.mallari@ringcentral.com>
2019-05-26 01:48:34 -0700

    added send email script and cleaned scrapefolder script

--- scrapefolders.py
+++ scrapefolders.py
@@ -32,8 +32,12 @@ import csv
 
 
 CLIENT_SECRET = "/Users/eyana.mallari/Projects-Local/client_secret_eyanag.json"
-
-
+FOLDER_ID = '19FBo4iSjyCS3NcqX6zaNgxtXWMqW7AyM'
+SS_ID = '1qs2GXzxhbn8klcB3Y-qQVJs0CRt8iQ20LyntKP36BRY'
+SHEET_ID=227869185
+TAB_NAME ='Folders Lookup!A2:H'
+CHAR_BEFORE_NAME = 16
+now = datetime.datetime.now()
 
 # --------------------------------
 # GDrive API: GDrive Authorization
@@ -49,23 +53,15 @@ DRIVE_SERVICE = build('drive', 'v3', http=creds.authorize(Http()))
 SS_SERVICE = build('sheets', 'v4', http=creds.authorize(Http()))
 
 
-FOLDER_ID = '19FBo4iSjyCS3NcqX6zaNgxtXWMqW7AyM'
-SS_ID = '1qs2GXzxhbn8klcB3Y-qQVJs0CRt8iQ20LyntKP36BRY'
-SHEET_ID=227869185
-TAB_NAME ='Folders Lookup!A2:H'
-CHAR_BEFORE_NAME = 16
-now = datetime.datetime.now()
+
 
 def countFiles(folder_id):
-# Main Folder
-
     results = DRIVE_SERVICE.files().list(q="mimeType='application/vnd.google-apps.spreadsheet' and parents in '"+folder_id+"' and trashed = false",fields="nextPageToken, files(id, name)").execute()
 
     items = results.get('files', [])
 
     count = 0
     for item in items:
-        #print('{0} ({1})'.format(item['name'], item['id']))
         count += 1
     return count
 
@@ -73,17 +69,14 @@ def getLatestFile(folder_id):
 # Main Folder
 
     results = DRIVE_SERVICE.files().list(q="mimeType='application/vnd.google-apps.spreadsheet' and parents in '"+folder_id+"' and trashed = false",fields="nextPageToken, files(id, name, createdTime)", orderBy="createdTime").execute()
-
-
     #results = DRIVE_SERVICE.files().list(q="parents in '"+folder_id+"' and trashed = false",fields="nextPageToken, files(id, name, createdTime)", orderBy="createdTime").execute()
-
-
     items = results.get('files', [])
 
     if not items:
         return ""
     else:
         return items
+
 def retrieveId(items,count):
     if not items:
         return ""
@@ -121,7 +114,6 @@ def writeToSheets(values,range,spreadsheet_id):
 # GDrive API: Retrieves all Folders
 # -------------------------------------
 def getAllFolders(folder_id, spreadsheet_id):
-# Main Folder
     folder_id = FOLDER_ID
 
     results = DRIVE_SERVICE.files().list(q="mimeType='application/vnd.google-apps.folder' and parents in '"+folder_id+"' and trashed = false",fields="nextPageToken, files(id, name)",pageSize=400).execute()
@@ -129,17 +121,15 @@ def getAllFolders(folder_id, spreadsheet_id):
     items = results.get('files', [])
 
     values = []
-    #print(type(items))
-    #print(items)
 
     count = 1
     print(len(items))
+
     for item in items:
         print(count)
         print('{0} ({1})'.format(item['name'], item['id']))
 
         person_name = item['name'][CHAR_BEFORE_NAME:].strip()
-
 
         file_count = countFiles(item['id'])
         latestFileId = '=HYPERLINK("https://docs.google.com/spreadsheets/d/'+retrieveId(getLatestFile(item['id']),file_count)+'","Click")'
@@ -153,7 +143,6 @@ def getAllFolders(folder_id, spreadsheet_id):
 
         item['person'] = person_name
         item['file_count'] = file_count
-        #item['link'] ="https://drive.google.com/drive/folders/"+item['id']
         item['link'] = '=HYPERLINK("https://drive.google.com/drive/folders/'+item['id']+'","Click")'
 
         values.append([item['person'],item['name'],item['link'],item['id'],item['file_count'],latestFile,latestFileId,createdDate])
@@ -161,9 +150,6 @@ def getAllFolders(folder_id, spreadsheet_id):
 
         count+=1
     writeToSheets(values,TAB_NAME,spreadsheet_id)
-
-
-
 
 
 def colorAndSort(spreadsheet_id):
@@ -229,7 +215,6 @@ def colorAndSort(spreadsheet_id):
 def main():
     getAllFolders(FOLDER_ID,SS_ID)
     colorAndSort(SS_ID)
-    #loopRosterCreateFiles(getSalesRep())
 
 if __name__ == '__main__':
     main()
---
+++ sendemails.py
@@ -0,0 +1,87 @@
+import smtplib
+import csv
+from string import Template
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+import datetime
+from email import encoders
+import imaplib
+import time
+
+
+CREDENTIALS_USER ="eyana.mallari@rcoffice.ringcentral.com"
+CREDENTIALS_PASS = "x"
+EMAIL_FROM_DEFAULT = "eyana.mallari@ringcentral.com"
+
+EMAIL_CC_DEFAULT = ""
+EMAIL_BCC_DEFAULT = ""
+
+CONTACTS_FILE = "/Users/eyana.mallari/Projects-Local/py_teamlink/Input/CONTACTS SAMPLE2.csv"
+
+
+def read_template(filename):
+
+    with open(filename, 'r') as template_file:
+        template_file_content = template_file.read()
+        print(template_file_content)
+    return template_file_content
+
+
+def getEmailContent(first_name):
+
+    email_content ="""
+Hi """+first_name+""",
+<br><br>
+Email content goes here
+<br><br>
+Regards,<br>
+Eyana
+
+    """
+
+    return email_content
+
+def loop_contacts(filename):
+    print('looping contacts')
+
+    # set up the SMTP server
+    print('Sending emails')
+    s = smtplib.SMTP(host='smtp.office365.com', port=587)
+    s.starttls()
+    s.login(CREDENTIALS_USER, CREDENTIALS_PASS)
+
+
+    count = 1
+
+    with open(filename, mode='r') as contacts_file:
+        reader = csv.reader(contacts_file)
+        next(reader)
+        for contact in reader:
+            contact_full_name = contact[0]
+            company = contact[1]
+            title = contact[2]
+            email = contact[3]
+            first_name= contact[4]
+
+            msg = MIMEMultipart()
+
+            print(count)
+            count = count +1
+            print("Sending email to", contact_full_name)
+
+            msg['From']=EMAIL_FROM_DEFAULT
+            msg['To']=email
+            msg['Bcc'] = EMAIL_BCC_DEFAULT
+            msg['Subject']=  EMAIL_SUBJECT
+
+            msg.attach(MIMEText(getEmailContent(first_name), 'html'))
+            print(type(msg))
+            s.send_message(msg)
+
+            del msg
+
+    s.quit()
+
+loop_contacts(CONTACTS_FILE)
